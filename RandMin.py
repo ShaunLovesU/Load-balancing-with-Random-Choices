58a2249164b6d3ba1009de80441c7037c5bc94b6
ShaunLovesU <shawn.zhang3114535672@gmail.com>
2023-04-04 13:29:54 -0700

    Final RandMin balancer

--- RandMin.py
+++ RandMin.py
@@ -9,93 +9,86 @@ import math
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Parameters
-RANDOM_SEED = 204204
-NUM_SERVERS = 5  # Number of servers in the system
-Mean_interarrival = 2  # Exponential distribution mean
-Mean_service = 20  # Exponential distribution mean
-total_customers = 300  # Total number of customers
 
+class  QSYSTEM:
+    def __init__(self, rand_seed, servers_n, mean_arrtime, mean_sertime, customers_n):
+        self.rand_seed = rand_seed
+        self.servers_n = servers_n
+        self.mean_arrtime = mean_arrtime
+        self.mean_sertime = mean_sertime
+        self.customers_n = customers_n
+        self.queues = []
+    def source(self, environment, servers):
+        """Source generates customers randomly"""
+        for i in range(self.customers_n):
+            c = self.customer(environment, 'Customer#%02d' % i, servers)
+            environment.process(c)
+            each_arrival = random.expovariate(1.0 / self.mean_arrtime)
+            yield environment.timeout(each_arrival) 
+            
+    def customer(self, environment, name, servers):
+        """The customer arrives, dispatch customer to a shortest queue."""
+        arrive = environment.now
+        print('%7.4f %s: Arrived' % (arrive, name))
+        
+        num_selected_server = random.randint(1, self.servers_n-1)
+        balancer_lst = random.sample(range(len(servers)), num_selected_server)
+        
+        each_length = {}
+        for bi in balancer_lst:
+            each_length[bi] = len(servers[bi].put_queue) + len(servers[bi].users)           
+            
+        tmp = {}
+        for i in range(len(servers)):
+            tmp[i] = len(servers[i].put_queue)
+        self.queues.append(tmp)
 
-Queues = []
+        sort_each_length = sorted(each_length.items(), key = lambda x : x[1])
+        choice = sort_each_length[0][0]
+        with servers[choice].request() as req:
+            yield req
+            wait = environment.now - arrive
+            print('%7.4f %s: Waited %6.3f, start service' % (environment.now, name, wait))
 
-def load_balancer(n,servers):
-    global Queues
-    return random.sample(range(len(servers)), n)
+            service_time = random.expovariate(1.0 / self.mean_sertime)
+            yield environment.timeout(service_time)
+            print('%7.4f %s: departure' % (environment.now, name))
+            tmp = {}
+            for i in range(len(servers)):
+                tmp[i] = len(servers[i].put_queue)
+            self.queues.append(tmp)
 
-def NoInSystem(R):
-    """Total number of jobs in the resource R"""
-    global Queues
-    return len(R.put_queue) + len(R.users)
+    def draw(self):
+        df = pd.DataFrame(self.queues).plot()
+        plt.title('Queue length')
+        plt.xlabel('Time')
+        plt.show()
+    def creat_environment(self):
+        print('m/m/m queueing system with load balancer (RandMin)')
+        random.seed(self.rand_seed)
+        environment = simpy.Environment()    
+        servers = []
+        for i in range(self.servers_n):
+            servers.append(simpy.Resource(environment, capacity=1))
+        environment.process(self.source(environment, servers))
 
-def customer(env, name, servers):
-    """The customer arrives, dispatch customer to a shortest queue."""
+        print('Running simulation...')
+        environment.run(until = 100000)
 
-    global Queues
-    arrive = env.now
-    print('%7.4f %s: Arrived' % (arrive, name))
-
-    # get the random num of servers
-    num_selected_server = random.randint(1, NUM_SERVERS-1)
-    # get selected server's queue length
-    each_length = {i: NoInSystem(servers[i]) for i in load_balancer(num_selected_server, servers)}
-
-    # need to change
-    Queues.append({i: len(servers[i].put_queue) for i in range(len(servers))})
-    
-
-    choice = [k for k, v in sorted(each_length.items(), key=lambda a: a[1])][0]
-
-    with servers[choice].request() as req:
-        yield req
-
-        wait = env.now - arrive
-        print('%7.4f %s: Waited %6.3f, start service' % (env.now, name, wait))
-
-        service_time = random.expovariate(1.0 / Mean_service)
-        yield env.timeout(service_time)
-        print('%7.4f %s: departure' % (env.now, name))
-
-        # for i in range(len(servers)):
-        #     Queues.append({i: len(servers[i].put_queue)})
-        Queues.append({i: len(servers[i].put_queue) for i in range(len(servers))})
-
-
-
-
-def source(env, number, interval, servers):
-    """Source generates customers randomly"""
-    global Queues
-    for i in range(number):
-        c = customer(env, 'Customer#%02d' % i, servers)
-
-        env.process(c)
-        each_arrival = random.expovariate(1.0 / interval)
-        yield env.timeout(each_arrival)
-
+        print('finish simulation')
+        return environment
 
 def main():
-    global Queues
-    print('m/m/m queueing system with load balancer (RandMin)')
-    random.seed(RANDOM_SEED)
-    env = simpy.Environment()
-
-   
-    servers = [simpy.Resource(env, capacity=1) for i in range(NUM_SERVERS)]
-    env.process(source(env, total_customers, Mean_interarrival, servers))
-
-    print('Running simulation...')
-    env.run(until = 100000)
-
-    print('finish simulation')
-
-    # print(Queues)
-
-    df = pd.DataFrame(Queues).plot()
-    plt.title('Queue length over time')
-    plt.xlabel('Time')
-    plt.ylabel('Queue length')
-    plt.show()
+    # Parameters
+    RSEED = 204204
+    SERVERS_N = 5  # Number of servers in the system
+    mean_arrtime = 2  # Exponential distribution mean
+    mean_sertime = 20  # Exponential distribution mean
+    customers_n = 300  # Total number of customers
+    
+    qs = QSYSTEM(RSEED, SERVERS_N, mean_arrtime, mean_sertime, customers_n)
+    qs.creat_environment()
+    qs.draw()
 
     exit(0)
 
